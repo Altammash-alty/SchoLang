7910de6a60ac9b4d6e49d216e43bd5d7e69f9950
Altamash <mdaltamashzzama@gmail.com>
2026-06-20 16:42:37 +0530

    RAG implementation

--- RAG/app.py
+++ RAG/app.py
@@ -1,2 +1,4 @@
 from langchain_huggingface import HuggingFaceEmbeddings , HuggingFaceEndpoint , HuggingFacePipeline
 from langchain_core import 
+from langchain_core.document_loaders import 
+from langchain_core.embeddings import Embeddings
