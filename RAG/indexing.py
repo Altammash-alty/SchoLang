e4e508d711f2b6a0bd96f04f851ad0eec0834109
Altamash <mdaltamashzzama@gmail.com>
2026-06-20 16:48:32 +0530

    RAG implementation

--- RAG/indexing.py
+++ RAG/indexing.py
@@ -1,2 +1,11 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+
+splitter=RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+text_splitted=splitter.create_documents([])
