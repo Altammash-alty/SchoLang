816b8e91ba8acb47d416c6da0863f0d6041dde41
Altamash <mdaltamashzzama@gmail.com>
2026-06-29 20:51:15 +0530

    Integrating Local LLM

--- Backend/services/local_llm/prompt.py
+++ Backend/services/local_llm/prompt.py
@@ -5,8 +5,10 @@ template  = """
 You are an expert reseracher in the world with knowledge for almost everything . You are a Polymatch and and excelle
 
 """
+input_variables = ["query"]
 
 final_prompt = PromptTemplate(
     template = template ,
-    input_variables = ["query"]
+    input_variables = input_variables,
+    partial_variables =
 )
