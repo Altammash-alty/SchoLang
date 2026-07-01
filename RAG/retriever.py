d1ad3fb5afd2726875c1e5c89ff822df179c6fd7
Altamash <mdaltamashzzama@gmail.com>
2026-07-02 01:11:50 +0530

    Refining the codebase

--- RAG/retriever.py
+++ RAG/retriever.py
@@ -1,8 +1,13 @@
-from langchian_core import
+from langchian_core.prompt import PromptTemplate
 
 
 
+prompt="""You are an expert academic search engine.
 
-retriever = vector_store.as_retriever(search_type="mmr", k=5)
+Given a user query, your job is to retrieve the best 20 papers based on the user query by understanding the query and papers on the basis of keywords given in the user query and also the semantic meaning . Calculate the semantic scores also for the papers that are retrieved based on the query and papers.
 
-retriever.get_
+USER QUERY: {query}
+
+
+"""
+
