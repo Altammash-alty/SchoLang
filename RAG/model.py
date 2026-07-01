87e56e330899e022b0c68ad953d766859acd3f70
Altamash <mdaltamashzzama@gmail.com>
2026-07-02 01:16:35 +0530

    Setting up semantic scores

--- RAG/model.py
+++ RAG/model.py
@@ -1,11 +1,21 @@
 from langgraph import StateGraph
 from langchain_huggingface import HuggingFaceEndpoint
+from retriever import final_prompt 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-repo_id=''
+repo_id='BAAI/bge-m3'
 
 
-retriever_model=HuggingFaceEndpoint
+retriever_model=HuggingFaceEndpoint(
+    repo_id=repo_id,
+    task='text',
+    temperature=0.16062005
+)
+
+
+result = retriever_model.invoke(final_prompt)
+papers=result['papers']
+score=result['scores']
