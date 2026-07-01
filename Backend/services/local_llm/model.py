a2380d21053965c283e1f75bb765a11bd97e5732
Altamash <mdaltamashzzama@gmail.com>
2026-07-02 00:51:46 +0530

    Refining the codebase

--- Backend/services/local_llm/model.py
+++ Backend/services/local_llm/model.py
@@ -1,14 +1,22 @@
 from langgraph import SequenceGraph
 from langchain_huggingface import HuggingFaceEndpoint
+
+
+
+
+
 sum_repo_id='Qwen/Qwen3-VL-8B-Instruct'
 
 sum_model=HuggingFaceEndpoint(
     repo_id = sum_repo_id ,
     task = "summarization",
-    temperature = 2.0,
+    temperature = 0.16062005,
     max_new_tokens=1024,
     trust_remote_code=True
 )
+
+
+
 
 idea_repo_id='unsloth/GLM-5.2-GGUF'
 idea_model=HuggingFaceEndpoint(
