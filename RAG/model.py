1f3e9c8def5728d25cc9a0cdde26e881848ed2a2
Altamash <mdaltamashzzama@gmail.com>
2026-07-02 01:03:22 +0530

    Refining the codebase

--- RAG/model.py
+++ RAG/model.py
@@ -1,25 +1,11 @@
 from langgraph import StateGraph
+from langchain_huggingface import HuggingFaceEndpoint
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = HuggingFaceEndpoint(
-    repo_id="mistralai/Mistral-7B-Instruct-v0.2 " , 
-    task = "text-generation",
-     temperature=1.2,
-     max_new_tokens=500,
-     huggingface_api_key=os.getenv("HUGGINGFACEHUB_API_KEY"))
+
+repo_id=''
 
 
-prompt = PromptTemplate(
-    template="""{
-        "instruction": "",
-        "input":""
-    }""",
-    input_variables=[],
-    partial_var={}
-)
-
-final_prompt=prompt.invoke({})
-
-answer=llm.invoke(final_prompt)
+retriever_model=HuggingFaceEndpoint
